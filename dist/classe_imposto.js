98463d0b1284b7efb864ec9b371e3f71a538ddc9
felipeyutani <82685484+felipeyutani@users.noreply.github.com>
2021-09-10 18:18:07 -0300

    Add files via upload

---
+++ dist/classe_imposto.js
@@ -0,0 +1,158 @@
+"use strict";
+/* 4) Crie uma classe para calcular o imposto de renda. Crie uma classe Pai com os
+atributos nome e renda anual. Crie os getters e setters com as suas validações. Crie
+uma classe filha Pessoa Física para herdar da classe Pai e com o atributo gastos
+com saúde, crie os getter e setter com a validação. Crie uma classe filha Pessoa
+Jurídica para herdar da classe Pai e com o atributo número de funcionários. Crie o
+getter e o setter com a validação.
+Regras para os cálculos
+Pessoa Física: Pessoas cuja renda foi abaixo de 20.000,00 pagam 15% de imposto.
+Pessoas com renda maior ou igual a 20.000,00 pagam imposto de 25%. Se a
+pessoa teve gastos com saúde, 50% destes gastos são abatidos no imposto.
+Exemplo: Se uma pessoa cuja renda foi de 50.000,00 e teve 2.000,00 em gastos
+com saúde, o imposto fica: (50.000,00 * 25%) - (2000 * 50%) = 11.500,00
+Pessoa Jurídica: Pessoas jurídicas pagam 16% de imposto, porém, se a empresa
+possuir mais de 10 funcionários, ela paga 14% de imposto. Exemplo: Uma empresa
+Trabalho de POO II 2
+cuja renda foi de 400.000,00 e possui 25 funcionários, o imposto fica: 400.000,00 *
+14% = 56.000,00 */
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Imposto = /** @class */ (function () {
+    function Imposto(nome, renda_anual) {
+        this._nome = nome;
+        this._renda_anual = renda_anual;
+    }
+    Object.defineProperty(Imposto.prototype, "nome", {
+        get: function () {
+            return this._nome;
+        },
+        set: function (nome) {
+            if (nome === '') {
+                throw new Error('Nome inválido, preencha o campo!');
+            }
+            this._nome = nome;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Imposto.prototype, "renda_anual", {
+        get: function () {
+            return this._renda_anual;
+        },
+        set: function (renda_anual) {
+            if (renda_anual === 0) {
+                throw new Error('Renda anual inválida, preencha o campo!');
+            }
+            this._renda_anual = renda_anual;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Imposto;
+}());
+var Pessoa_fisica = /** @class */ (function (_super) {
+    __extends(Pessoa_fisica, _super);
+    function Pessoa_fisica(nome, renda_anual, gasto_saude) {
+        var _this = _super.call(this, nome, renda_anual) || this;
+        _this._gasto_saude = gasto_saude;
+        return _this;
+    }
+    Object.defineProperty(Pessoa_fisica.prototype, "gasto_saude", {
+        get: function () {
+            return this._gasto_saude;
+        },
+        set: function (gasto_saude) {
+            if (gasto_saude = 0) {
+                throw new Error('Gasto com saúde inválida, preencha o campo!');
+            }
+            this._gasto_saude = gasto_saude;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Pessoa_fisica.prototype.calculoImpostoFI = function () {
+        if (this.renda_anual <= 20000) {
+            return (this.renda_anual * 0.15) - (this.gasto_saude * 0.5);
+        }
+        else {
+            return (this.renda_anual * 0.25) - (this.gasto_saude * 0.5);
+        }
+    };
+    return Pessoa_fisica;
+}(Imposto));
+var Pessoa_juridica = /** @class */ (function (_super) {
+    __extends(Pessoa_juridica, _super);
+    function Pessoa_juridica(nome, renda_anual, numero_func) {
+        var _this = _super.call(this, nome, renda_anual) || this;
+        _this._numero_func = numero_func;
+        return _this;
+    }
+    Object.defineProperty(Pessoa_juridica.prototype, "numero_func", {
+        get: function () {
+            return this._numero_func;
+        },
+        set: function (numero_func) {
+            if (numero_func = 0) {
+                throw new Error('Numero de funcionários inválido, preencha o campo corretamente!');
+            }
+            this._numero_func = numero_func;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Pessoa_juridica.prototype.calculoImpostoJR = function () {
+        if (this.numero_func <= 10) {
+            return this.renda_anual * 0.16;
+        }
+        else {
+            return this.renda_anual * 0.14;
+        }
+    };
+    return Pessoa_juridica;
+}(Imposto));
+var imposto = new Imposto('José', 20000);
+var pessoa_fisica = new Pessoa_fisica('Maria', 50000, 2000);
+var pessoa_juridica = new Pessoa_juridica('Vanderson', 400000, 50);
+console.log("Imposto pessoa fisica: " + pessoa_fisica.calculoImpostoFI());
+console.log("Imposto pessoa juridica: " + pessoa_juridica.calculoImpostoJR());
+/* try {
+    imposto.nome = 'ss'
+    console.log(imposto)
+} catch(error) {
+    console.log(error.message)
+}
+
+try {
+    imposto.renda_anual = 5
+    console.log(imposto)
+} catch(error) {
+    console.log(error.message)
+}
+
+try {
+    pessoa_fisica.gasto_saude = 5
+    console.log(pessoa_fisica)
+} catch(error) {
+    console.log(error.message)
+}
+
+try {
+    pessoa_juridica.numero_func = 5
+    console.log(pessoa_juridica)
+} catch(error) {
+    console.log(error.message)
+} */ 
---
+++ src/classe_vendedores.ts
@@ -0,0 +1,226 @@
+/* 5) Crie uma classe chamada Vendedores com os atributos nome, salario e valor da
+venda. Crie os getters e setters e as validações. Crie um método desconto para
+calcular 8% do salário.
+Crie uma classe filha chamada Vendedor de Pessoa Física com o atributo região,
+crie o getter e setter com validação para aceitar somente as regiões sul, sudeste,
+centro-oeste, norte e nordeste. Crie um método para calcular a comissão. Se a
+região for sul a comissão será de 10% sobre o valor da venda. Se a região for
+sudeste a comissão será de 12% sobre o valor da venda, se a região for centrooeste a comissão será de 14% sobre o valor da venda, se a região for norte a
+comissão será de 15% sobre o valor da venda, Se a região for nordeste a comissão
+será de 17% sobre o valor da venda. Crie o método para calcular o salário total que
+será o salario mais a comissão.
+Crie uma classe filha chamada Pessoa Jurídica com os atributos nome da empresa,
+total de funcionários, crie os getters, setters e as suas validações. Crie um método
+para calcular a comissão. Se o valor da venda for menor que 5.000,00 o valor da
+comissão será de 2% sobre o valor da venda. Se o valor da venda for maior ou igual
+a 5.000,00 e menor que 10.000,00 o valor da comissão será de 4% sobre o valor da
+venda. Se o valor da venda for maior ou igual a 10.000,00 o valor da comissão será
+de 6% sobre o valor da venda. Crie um método salario Total que será a soma do
+salario mais comissão e mais R$ 200,00 se o número de funcionários for menor que
+100 ou mais R$ 300,00 se o número de funcionários for maior ou igual a 100.
+ */
+
+class Vendedores {
+    public _nome: string;
+    public _salario: number;
+    public _valor_venda: number;
+
+    constructor(nome: string, salario: number, valor_venda: number) {
+        this._nome = nome,
+        this._salario = salario,
+        this._valor_venda = valor_venda
+    }
+
+    get nome() {
+        return this._nome
+    }
+
+    get salario() {
+        return this._salario
+    }
+
+    get valor_venda(){
+        return this._valor_venda
+    }
+
+    set nome(nome: string){
+        if(nome === '') {
+            throw new Error('Nome inválido, preencha o campo!')
+        }
+        this._nome = nome
+    }
+
+    set salario(salario: number){
+        if(salario === 0) {
+            throw new Error('Salário inválido, preencha o campo!')
+        }
+        this._salario = salario
+    }
+
+    set valor_venda(valor_venda: number){
+        if(valor_venda === 0) {
+            throw new Error('Valor de venda inválido, preencha o campo!')
+        }
+        this._valor_venda = valor_venda
+    }
+
+    public desconto(){
+        return this.salario*0.08
+    }
+}
+
+class Vendedor_PF extends Vendedores {
+    static REGIOES = ['sul', 'sudeste', 'centro-oeste', 'norte', 'nordeste']
+    public _regiao: string;
+
+    constructor(nome: string, salario: number, valor_venda: number, regiao: string){
+        super(nome, salario, valor_venda)
+        this._regiao = regiao
+    }
+
+    get regiao(){
+        return this._regiao
+    }
+
+    set regiao(regiao: string){
+        if(!Vendedor_PF.REGIOES.includes(regiao)) {
+            throw new Error('Região Inválida!')
+        }
+        this._regiao = regiao 
+    }
+
+    public comissao(){
+        if(this.regiao === 'sul'){
+            return this.valor_venda*0.10
+        } else if(this.regiao === 'sudeste'){
+            return this.valor_venda*0.12
+        } else if(this.regiao === 'centro-oeste'){
+            return this.valor_venda*0.14
+        } else if(this.regiao === 'norte'){
+            return this.valor_venda*0.15
+        } else if(this.regiao === 'nordeste'){
+            return this.valor_venda*0.17
+        } else {}
+    }
+
+    public salarioTotal(){
+        if(this.regiao === 'sul'){
+            return this.valor_venda*0.10 + this.salario
+        } else if(this.regiao === 'sudeste'){
+            return this.valor_venda*0.12 + this.salario
+        } else if(this.regiao === 'centro-oeste'){
+            return this.valor_venda*0.14 + this.salario
+        } else if(this.regiao === 'norte'){
+            return this.valor_venda*0.15 + this.salario
+        } else if(this.regiao === 'nordeste'){
+            return this.valor_venda*0.17 + this.salario
+        } else {}
+    }
+}
+
+class Pessoajuridica extends Vendedores{
+    public _nome_empresa: string;
+    public _total_func: number;
+
+    constructor(nome: string, salario: number, valor_venda: number, nome_empresa: string, total_func: number){
+        super(nome, salario, valor_venda)
+        this._nome_empresa = nome_empresa
+        this._total_func = total_func
+    }
+
+    get nome_empresa(){
+        return this._nome_empresa
+    }
+
+    get total_func(){
+        return this._total_func
+    }
+
+    set nome_empresa(nome_empresa: string){
+        if(nome_empresa === '') {
+            throw new Error('Nome da empresa inválido, preencha o campo!')
+        }
+        this._nome_empresa = nome_empresa
+    }
+
+    set total_func(total_func: number){
+        if(total_func === 0) {
+            throw new Error('Total de funcionários inválido, preencha o campo!')
+        }
+        this._total_func = total_func
+    }
+
+    public comissaoPJ(): number{
+        if(this.valor_venda < 5000){
+            return this.valor_venda*0.02
+        } else if(this.valor_venda >= 5000 && this.valor_venda < 10000){
+            return this.valor_venda*0.04
+        } else if(this.valor_venda >= 10000){
+            return this.valor_venda*0.06
+        } else {}
+        return this.valor_venda
+    }
+
+    public salarioTotalPJ(){
+        if(this.total_func < 100){
+            return this.comissaoPJ() + this.salario + 200
+        } else if(this.total_func >= 100){
+            return this.comissaoPJ() + this.salario + 300
+        } else {}
+    }
+}
+
+
+let vendedores = new Vendedores('João', 10000, 500)
+let vendedorpf = new Vendedor_PF('Pedro', 20000, 3000, 'nordeste')
+let pessoajuridica = new Pessoajuridica('Josefina', 20000, 4000, 'Google', 100)
+
+
+console.log(`Desconto: ${vendedores.desconto()}`)
+
+console.log(`Comissão pela região: ${vendedorpf.comissao()}`)
+console.log(`Salário total: ${vendedorpf.salarioTotal()}`)
+console.log(`Comissão pelo valor de venda: ${pessoajuridica.comissaoPJ()}`)
+console.log(`Salário total pelo total de funcionários: ${pessoajuridica.salarioTotalPJ()}`)
+
+/* try {
+    vendedores.nome = ''
+    console.log(vendedores)
+} catch(error) {
+    console.log(error.message)
+}
+
+try {
+    vendedores.salario = 0
+    console.log(vendedores)
+} catch(error) {
+    console.log(error.message)
+}
+
+try {
+    vendedores.valor_venda = 0
+    console.log(vendedores)
+} catch(error) {
+    console.log(error.message)
+}
+
+try {
+    vendedorpf.regiao = ''
+    console.log(pessoa_fisica)
+} catch(error) {
+    console.log(error.message)
+}
+
+try {
+    pessoajuridica.nome_empresa = ''
+    console.log(pessoajuridica)
+} catch(error) {
+    console.log(error.message)
+}
+
+try {
+    pessoajuridica.total_func = 0
+    console.log(pessoajuridica)
+} catch(error) {
+    console.log(error.message)
+} */
